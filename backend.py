e72a6bf126ecb23d6e774069258e0966e6a17a71
= <n42950m@proton.me>
2024-02-05 20:14:34 +0000

    added creation of temp dirs and changed ffmpeg settings

--- backend.py
+++ backend.py
@@ -1,15 +1,19 @@
 #the logic for gifski and ffmpeg
 import ffmpeg
 import cv2
+import os
 
-def trim_and_encode(filepath, subtitle_track, audio_track, start_time, end_time):
+def get_resolution(filepath):
+    video_opencv = cv2.VideoCapture(filepath)
+    height = round(video_opencv.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    width = round(video_opencv.get(cv2.CAP_PROP_FRAME_WIDTH))
+    video_opencv.release()
+    video_resolution = f"{width}x{height}"
+    return video_resolution
+
+def trim_and_encode(filepath, subtitle_track, audio_track, start_time, end_time, video_resolution):
     #takes a file, cuts it from a certain time and reencodes
     if subtitle_track != "":
-        video_opencv = cv2.VideoCapture(filepath)
-        height = round(video_opencv.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        width = round(video_opencv.get(cv2.CAP_PROP_FRAME_WIDTH))
-        video_opencv.release()
-        video_resolution = f"{width}x{height}" #need this for .ass subtitle handling, https://ffmpeg.org/ffmpeg-utils.html#video-size-syntax
         file_in = ffmpeg.input(filepath)
         video = file_in.video.filter('subtitles', filepath, video_resolution, None, False, "UTF-8", subtitle_track)
         audio = file_in[f'a:{audio_track}']
@@ -20,7 +24,7 @@ def trim_and_encode(filepath, subtitle_track, audio_track, start_time, end_time)
             **{"codec:v": "libx264"}, 
             **{"codec:a": "copy"}, 
             crf=15, 
-            preset="ultrafast", 
+            preset="veryslow", 
             ss=start_time,
             to=end_time
         )
@@ -36,16 +40,19 @@ def trim_and_encode(filepath, subtitle_track, audio_track, start_time, end_time)
             **{"codec:v": "libx264"}, 
             **{"codec:a": "copy"}, 
             crf=15, 
-            preset="ultrafast", 
+            preset="veryslow", 
             ss=start_time,
             to=end_time
         )
         file_out.run()
 
 if __name__ == "__main__":
-    filepath = r"/home/angry/Downloads/testvideo.mkv"
-    subtitle_track = 3 #stars from 0
-    audio_track = 3 #starts from 0
+    filepath = r"/home/angry/Media/test2.mkv"
+    subtitle_track = 0 #stars from 0
+    audio_track = 0 #starts from 0
     start_time = "00:14:23.00"
     end_time = "00:14:30.00"
-    trim_and_encode(filepath,subtitle_track,audio_track,start_time,end_time)
+    video_resolution = get_resolution(filepath)
+    os.mkdir("temporary-directory")
+    os.chdir("temporary-directory")
+    trim_and_encode(filepath,subtitle_track,audio_track,start_time,end_time, video_resolution)
